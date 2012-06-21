bc8b8aacde9c941ada1cf751f31fef0f221b1f72
iProcrastinate <alexio2@mac.com>
2012-06-21 15:33:31 -0400

    centered carousel images and distinguished between users with and without gravatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,12 +15,21 @@ module UsersHelper
 ##      end
 ##  end
       
-  
+  def has_avatar?(user)
+    unless gravatar_for(user).nil? || user.avatar_file_name?
+    end
+  end
   
   def gravatar_for(user)
-    default_url = ""
+    require 'net/http'
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
-    image_tag(gravatar_url, alt: user.first_name, class: "gravatar")
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{404}"
+    uri= URI(gravatar_url)
+    res = Net::HTTP.get_response(uri)
+    if res.is_a? (Net::HTTPNotFound)
+      nil
+    else
+      image_tag(gravatar_url, alt: user.first_name, class: "gravatar")
+    end  
   end
 end
