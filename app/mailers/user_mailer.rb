8832ce4ca5de721fb1d3f7db62d114a2ee125508
iProcrastinate <alexio2@mac.com>
2012-06-27 16:55:18 -0400

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "admin@tippingpointpartners.com"
+#  default from: "admin@tippingpointpartners.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
