9d16c1f2952a931bf742789fe7c93928fb10c901
iProcrastinate <alexio2@mac.com>
2012-06-27 15:53:26 -0400

    added host for mailer

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module TippingPointPartners
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_mailer.default_url_options = { :host => "tpp.herokuapp.com" }
+    
   end
 end
