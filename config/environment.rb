0d4a2a02cb1e38a584e5c467d7f79d10ac334671
iProcrastinate <alexio2@mac.com>
2012-06-27 16:45:44 -0400

    mailer congif

--- config/environment.rb
+++ config/environment.rb
@@ -14,3 +14,12 @@ TippingPointPartners::Application.initialize!
 #configure cancan for authorization
 #config.gem "cancan"
 
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app5523529@heroku.com'],
+  :password       => ENV['xjlovlcy'],
+  :domain         => 'heroku.com'
+}
+ActionMailer::Base.delivery_method = :smtp
