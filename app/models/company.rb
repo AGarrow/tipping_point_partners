14604dbb4affb33ec7f1ba5dc678d3d4d845b4a0
iProcrastinate <alexio2@mac.com>
2012-07-03 14:34:35 -0400

    s3

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,8 @@
 class Company < ActiveRecord::Base
   attr_accessible :name, :print_code, :avatar, :blurb
   has_many :users
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>"},
-   :storage => :s3,
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-    :path => "/:style/:id/:filename"
-    
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>"}
+  
   before_save do |company|
     company.name = company.name.titleize
   end
