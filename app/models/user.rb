65b47482d49a2b7f225565631e66c1b52ea95bfe
iProcrastinate <alexio2@mac.com>
2012-07-03 15:00:05 -0400

    s3

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,7 @@ class User < ActiveRecord::Base
     :small => "150x150#"
     },
     :storage => :s3,
-    :bucket => ENV[tipping_point_partners],
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :s3_credentials => "config/s3.yml",
     :path => "/:style/:id/:filename"
   
   ROLES = %w[admin company_admin employee ]
