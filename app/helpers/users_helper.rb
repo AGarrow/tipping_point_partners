6a22944e5f61c155dbaf2b3107950a13bc80b5cc
iProcrastinate <alexio2@mac.com>
2012-07-06 12:19:06 -0400

    user emails and company names appended for neater views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,13 @@ module UsersHelper
     name = [user.first_name,user.last_name].join(' ')
   end
   
+  def list_email (user)
+    if user.email.length > 26
+      user.email[0,22]<<".."
+    else
+      user.email
+    end
+  end
 ##  def avatar_for (user)
 ##    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 ##      if gravatar_id == 40
