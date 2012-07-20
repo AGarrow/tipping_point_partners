685c15629081553a7932b66830c13a840cb47af6
iProcrastinate <alexio2@mac.com>
2012-07-20 14:08:59 -0400

    store company avatars in s3

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,14 @@ class Company < ActiveRecord::Base
   attr_accessible :name, :print_code, :avatar, :blurb, :email_extension
   has_many :users, dependent: :destroy 
   has_many :announcements, dependent: :destroy
-  has_attached_file :avatar, :styles => { :large => "360x268>",:medium_ish => "160x120>", :medium => "200x200>", :thumb => "100x100>"}
+  has_attached_file :avatar, :styles => { 
+    :large => "360x268>",
+    :medium_ish => "160x120>", 
+    :medium => "200x200>", 
+    :thumb => "100x100>"},
+      :storage => :s3,
+      :s3_credentials => "config/s3.yml",
+      :path => "/:style/:id/:filename"
   
   validates :name, :email_extension, presence: true, uniqueness: true
   validates :print_code, presence: true
