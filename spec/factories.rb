467ffa69f062c38237600fba4e4a92ca7aca2337
iProcrastinate <alexio2@mac.com>
2012-07-25 14:51:48 -0400

    model tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,60 +2,24 @@
 
 FactoryGirl.define do 
   
-
-  
-  factory :admin, class: User do 
-    first_name             "alexi"
-    last_name              "garrow"
-    email                  "alexi0@apporchard.com"
-    password               "please"
-    password_confirmation  "please"
-    role                   "admin"
-
+  factory :company do |f|
+    f.name {|n|"#{n}"}
+    f.email_extension {|u| "#{u.name.gsub(" ","")}.com"}
+    f.print_code "#{Random.rand(1000000..9999999)}"
   end
-  
-  factory :company_admin, class: User do
-    first_name              "amanda"
-    last_name               "connel"
-    email                   "amanda@apporchard.com"
-    password                "please"
-    password_confirmation   "please"
-    role                    "company_admin"
+
+
+
+
+  factory :user do |f|
+    f.email {|l|"#{l}"}
+    f.first_name {|u|"#{u.email.partition("@")[0].partition("_")[0]}"}
+    f.last_name  {|u|"#{u.email.partition("@")[0].partition("_")[2]}"}
+    f.password "foobar"
+    f.password_confirmation {|x| x.password}
+#    f.company {|r| Company.find_by_email_extension(r.email.partition("@")[2])}
+    f.association :company
     
   end
-  
-  factory :employee, class: User do
-    first_name              "khalid"
-    last_name               "mills"
-    email                   "khalid1@apporchard.com"
-    password                "please"
-    password_confirmation   "please"
-    role                    "company_admin"
-
-  end
-
-  factory :appOrchard, class: Company do 
-    name                "app orchard"
-    email_extension     "apporchard.com"
-    print_code          "1234567"
-    blurb               "not your traditional incubator"
-    
-    factory :company_with_users do
-      ignore do 
-        users_count 3
-      end
-      
-      after(:create) do |user, evaluator|
-        FactoryGirl.create_list(:user, evaluator.users_count, user: user)
-      end
-    end
-  end
-  
-  factory :perka, class: Company do
-    name                "perka"
-    email_extension     "perka.com"
-    print_code          "7654321"
-  end
-
 end
 
