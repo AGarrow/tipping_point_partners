f8e7fa2bb0acfbae9dc71542dde94689aea8d13e
iProcrastinate <alexio2@mac.com>
2012-07-30 15:52:02 -0400

    abilities upgraded to cancan 2.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   
-  load_and_authorize_resource 
-
-  skip_authorize_resource :only => :validate
+  load_and_authorize_resource :except => :validate
 
   def index
     @users = User.all
