318212dd66443bb0c36553968bc519e6629eefdf
iProcrastinate <alexio2@mac.com>
2012-08-05 08:20:23 -0400

    question model tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    @question = Question.new(question: "do questions work?", answer: "well, they should!")
+  end
+  
+  subject{@question}
+  
+  it { should respond_to(:question)}
+  it { should respond_to(:answer)}
+  it {should be_valid}
+  
+  describe "validations" do 
+    it "should validate presence of question" do
+      @question.question = ''
+      @question.save
+      should_not be_valid
+    end
+    
+    it "should validate presence of answer" do
+      @question.answer = ''
+      @question.save
+      should_not be_valid
+    end
+    
+    it "should validate uniqueness of question" do
+      @question.save
+      @copy_question = @question.dup
+      @copy_question.should_not be_valid
+    end
+  end
 end
