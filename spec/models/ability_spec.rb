cb8fe09770bf33a37ef53b3bf883ee6d5acbb0c2
iProcrastinate <alexio2@mac.com>
2012-08-06 18:47:57 -0400

    ability testing almost complete

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -30,21 +30,35 @@ describe User do
       it {should be_able_to(:access, @all_users )}
       it {should be_able_to(:access, Question.new)}
       it {should be_able_to(:access, Announcement.new)}
-#     it {should_not be_able_to(:update, user,[:role])}
+#     it {should_not be_able_to(:update, :user,[:role])}
     end
     
     context "as company_admin" do
-        let(:user) {FactoryGirl.create(:user, :email => "company_administrator@edisonlabs.com",:role => 'company_admin')}
-        it {should be_able_to(:access, @edisonCO)}
-        it {should_not be_able_to(:access, @teslaCO)}
-        it {should be_able_to(:destroy, @edison1)}
+      let(:user) {FactoryGirl.create(:user, :email => "company_administrator@edisonlabs.com",:role => 'company_admin')}
+      it {should be_able_to(:access, @edisonCO)}
+      it {should_not be_able_to(:access, @teslaCO)}
+      it {should_not be_able_to(:access, @planetCO)}
+      it {should be_able_to(:destroy, @edison1)}
 #        it {should be_able_to(:update, @edison1, :role)}    
-        it {should_not be_able_to(:destroy, @edison2)}
+      it {should_not be_able_to(:destroy, @edison2)}
 #        it {should_not be_able_to(:update, @edison2, :role)} 
-        it {should be_able_to(:create, @edisonCO.announcements.create(title: 'herpa', content: 'derp'))}
-        it {should be_able_to(:destroy, @edisonCO.announcements.all)}
-        it {should be_able_to()}
-      
+      it {should be_able_to(:create, @edisonCO.announcements.create(title: 'herpa', content: 'derp'))}
+      it {should be_able_to(:destroy, @edisonCO.announcements.first)}
+#        it {should_not be_able_to(:update, :user,:role)}
+      it {should_not be_able_to(:access, Question.new)}
+    
+    end
+    
+    context "as employee" do
+      let(:user) {FactoryGirl.create(:user, :email => "humble_employee@planetexpress.com", :role => 'employee')}
+      it{should be_able_to(:update, :user)}
+      it{should be_able_to(:read, @all_users)}
+      it{should be_able_to(:read, @all_companies)}
+      it{should_not be_able_to(:create, Company.new)}
+      it{should_not be_able_to(:destroy, @planet1)}
+      it{should_not be_able_to(:update, @planetCO)}
+      it{should_not be_able_to(:access, @all_companies)}
+#       it{should_not be_able_to(:update, :user, :role)}
     end
   end
 end
