77d1634fb95bf55103ad437e9cbee6cd0870fc5a
iProcrastinate <alexio2@mac.com>
2012-09-19 20:30:29 -0400

    need to prettify company selection

--- spec/mailers/announcement_mailer_spec.rb
+++ spec/mailers/announcement_mailer_spec.rb
@@ -1,5 +1,16 @@
 require "spec_helper"
 
 describe AnnouncementMailer do
+	private
+
+		def read_fixture(action)
+			IO.readlines("#{FIXTURES_PATH}/mailers/user_mailer/#{action}")
+		end
+
+
+
+
+
+
   pending "add some examples to (or delete) #{__FILE__}"
 end
